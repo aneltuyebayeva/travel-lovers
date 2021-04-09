52dbd540ad08c8fdd368bdc049e7100b5d711b7d
Anel Tuyebayeva <aneltuyebayeva@96-80-105-165-static.hfc.comcastbusiness.net>
2021-04-09 16:53:56 -0400

    sign up and login form functions

---
+++ main.js
@@ -0,0 +1,49 @@
+const signUpForm = document.querySelector('.signup-form');
+const loginForm = document.querySelector('.login-form');
+
+signUpForm.addEventListener('submit', async(e) => {
+    e.preventDefault()
+
+    const name = document.querySelector('#signup-name').value
+    const email = document.querySelector('#signup-email').value
+    const password = document.querySelector('#signup-password').value
+
+    try {
+        const response = await axios.post('http://localhost:3001/user', {
+            name: name,
+            email: email,
+            password: password
+        })
+        console.log(response)
+        const userId = response.data.user.id
+        console.log(userId)
+        const userName = response.data.user.name
+        localStorage.setItem('userId', userId) 
+        localStorage.setItem('userName', userName) 
+    } catch (error) {
+        console.log(error)
+        alert(error)
+    }
+}) 
+
+loginForm.addEventListener('submit', async(e) => {
+    e.preventDefault()
+
+    const email = document.querySelector('#login-email').value
+    const password = document.querySelector('#login-password').value
+
+    try {
+        const response = await axios.post('http://localhost:3001/user/login', {
+            email: email,
+            password: password
+        })
+        const userId = response.data.id
+        console.log(response.data)
+        const userName = response.data.name
+        localStorage.setItem('userId', userId) 
+        localStorage.setItem('userName', userName) 
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+})
