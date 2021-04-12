ae32a0296d86bbc8d3243e45944118b89cf7133c
Anel Tuyebayeva <aneltuyebayeva@96-80-105-165-static.hfc.comcastbusiness.net>
2021-04-12 16:43:09 -0400

    changed backendURL to prod

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const signUpForm = document.querySelector('.signup-form');
 const loginForm = document.querySelector('.login-form');
 const profile = document.querySelector('.profileScreen');
@@ -5,12 +6,33 @@ const signUpScreen = document.querySelector('.signUpScreen');
 const loginScreen = document.querySelector('.loginScreen');
 const navLinks = document.querySelector('.nav-links');
 
+const switchToProfile = () => {
+    profile.classList.remove('hidden')
+    signUpScreen.classList.add('hidden')
+    loginScreen.classList.add('hidden')
+    navLinks.classList.remove('hidden')
+    let userName = document.querySelector('.userName')
+    let usersName = localStorage.getItem('userName')
+    if(usersName !== undefined) {
+        userName.innerText = `Welcome ${usersName}`
+    }
+}
 const switchToLogin = () => {
+    profile.classList.add('hidden')
+    signUpScreen.classList.add('hidden')
+    loginScreen.classList.remove('hidden')
+    navLinks.classList.add('hidden')
+}
+
+let signUpButton= document.querySelector('.signUpButton')
+const switchToSignUp = () => {
     profile.classList.add('hidden')
     signUpScreen.classList.remove('hidden')
     loginScreen.classList.add('hidden')
-    navLinks.classList.add('hidden')
 }
+signUpButton.addEventListener('click', () => {
+    switchToSignUp()
+})
 
 signUpForm.addEventListener('submit', async(e) => {
     e.preventDefault()
@@ -31,6 +53,7 @@ signUpForm.addEventListener('submit', async(e) => {
         const userName = response.data.user.name
         localStorage.setItem('userId', userId) 
         localStorage.setItem('userName', userName) 
+        switchToProfile()
     } catch (error) {
         console.log(error)
         alert(error)
@@ -53,8 +76,55 @@ loginForm.addEventListener('submit', async(e) => {
         const userName = response.data.name
         localStorage.setItem('userId', userId) 
         localStorage.setItem('userName', userName) 
+        switchToProfile()
     } catch (error) {
       console.log(error)
       alert(error)
     }
+})
+
+const searchForm = document.querySelector('.search-form')
+  searchForm.addEventListener('submit', async(e) => {
+    e.preventDefault()
+    try {
+     let searchBar = document.querySelector('#city-search').value
+      console.log(searchBar)
+      const response = await axios.post('http://localhost:3001/city/search', {
+          city: searchBar
+      })
+      console.log(response.data)
+      showResults(response.data)
+      searchForm.reset()
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  
+const showResults = (data) => {
+   
+    let searchResults = document.querySelector('.searchResults')
+    searchResults.classList.remove('hidden')
+    let showCity = document.querySelector('.showCityName')
+    showCity.classList.remove('hidden')
+
+    let searchBar = document.querySelector('#city-search').value
+    let showCityName = document.querySelector('show-city-name')
+    showCityName.innerText = searchBar
+
+    let showName = document.querySelector('.show-name')
+    showName.innerText = data.results[0].name
+}
+
+const loginButton = document.querySelector('.loginButton')
+
+loginButton.addEventListener('click', () => {
+    switchToLogin()
+})
+
+const logoutButton = document.querySelector('#logout-link')
+
+logoutButton.addEventListener('click', () => {
+    localStorage.clear()
+    switchToLogin()
 })
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const app = express()
-
 const routesReport = require('rowdy-logger').begin(app)
-
 const path = require('path')
+const replaceInFile = require('replace-in-file')
 
 app.get('/', (req, res) => {
   const filepath = path.join(__dirname, 'index.html')
   res.sendFile(filepath)
 })
 
-app.get('/main.js', (req, res) => {
+app.get('/main.js', async (req, res) => {
   const filepath = path.join(__dirname, 'main.js')
+  
+  if (process.env.NODE_ENV === 'production') {
+  await replaceInFile ({
+    files: filepath,
+    from: 'http://localhost:3001',
+    to: 'https://travel-lovers-app.herokuapp.com'
+  })
+}
   res.sendFile(filepath)
 })
 
