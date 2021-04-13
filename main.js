2679aafa2f2d972f0c1efdd997331e758e8b5400
Anel Tuyebayeva <aneltuyebayeva@96-80-105-165-static.hfc.comcastbusiness.net>
2021-04-13 15:03:34 -0400

    save function is working

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const profile = document.querySelector('.profileScreen');
 const signUpScreen = document.querySelector('.signUpScreen');
 const loginScreen = document.querySelector('.loginScreen');
 const navLinks = document.querySelector('.nav-links');
+const searchResults = document.querySelector('.searchResults')
 
 const switchToProfile = () => {
     profile.classList.remove('hidden')
@@ -82,7 +83,7 @@ loginForm.addEventListener('submit', async(e) => {
       alert(error)
     }
 })
-
+let searchLocation = ''
 const searchForm = document.querySelector('.search-form')
   searchForm.addEventListener('submit', async(e) => {
     e.preventDefault()
@@ -93,33 +94,56 @@ const searchForm = document.querySelector('.search-form')
           city: searchBar.charAt(0).toUpperCase()+searchBar.slice(1)
       })
       console.log(response.data)
-      showResults(response.data)
+      searchResults.innerHTML = ''
+           for (let place of response.data.results) {
+          let placeName = place.name
+          let placeImage = place.images[0].sizes.thumbnail.url
+          let placeDescription = place.snippet
+          
+          
+          showResults(placeName, placeImage, placeDescription)
+      }
+     
+
+    let showCityName = document.querySelector('.show-city-name')
+    showCityName.innerText = response.data.results[0].location_id
+    searchLocation = response.data.results[0].location_id
       searchForm.reset()
     } catch (error) {
       console.log(error)
     }
   })
 
-  
-const showResults = (data) => {
-   
-    let searchResults = document.querySelector('.searchResults')
+    // resultsId = null
+const showResults = (name, image, description) => {
+    // resultsId = data.results[0].location_id
+    
     searchResults.classList.remove('hidden')
     let showCity = document.querySelector('.showCityName')
     showCity.classList.remove('hidden')
 
-    let showCityName = document.querySelector('.show-city-name')
-    showCityName.innerText = data.results[0].location_id
+    let card = document.createElement('div')
+    let placeName = document.createElement('h2')
+    let placeImage = document.createElement('img')
+    let placeDescription = document.createElement('p')
+    
+    card.classList.add('place-card')
 
-    let showName = document.querySelector('.show-name')
-    showName.innerText = data.results[0].name
+    
 
-    let showImage = document.querySelector('#show-image')
-    showImage.src = data.results[0].images[0].sizes.thumbnail.url
+    // let placeName = document.querySelector('.show-name')
+    placeName.innerText = name
+
+    // let showImage = document.querySelector('#show-image')
+    placeImage.src = image
   
 
-    let showDescription = document.querySelector('.show-description')
-    showDescription.innerText = data.results[0].snippet
+    // let showDescription = document.querySelector('.show-description')
+    placeDescription.innerText = description
+
+    card.append(placeName, placeImage, placeDescription)
+    
+    searchResults.append(card)
 }
 
 const loginButton = document.querySelector('.loginButton')
@@ -133,4 +157,19 @@ const logoutButton = document.querySelector('#logout-link')
 logoutButton.addEventListener('click', () => {
     localStorage.clear()
     switchToLogin()
+})
+
+const saveSearch = document.querySelector('.saveSearch')
+saveSearch.addEventListener('click', async (e) => {
+    e.preventDefault()
+    
+    try {
+        let userId = localStorage.getItem('userId')
+  
+    const res = await axios.post(`http://localhost:3001/city/${userId}/save/${searchLocation}`)
+   
+    console.log(res)
+    } catch (error) {
+        console.log(res)
+    }
 })
